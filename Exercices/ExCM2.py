df006fcaba176716550491eca8fbe1a5dca45107
Gilles Poncelet <gillesponcelet98@gmail.com>
2021-02-15 15:31:47 +0100

    do exercise

---
+++ Exercices/ExCM2.py
@@ -0,0 +1,52 @@
+import warnings
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.fftpack import fft, ifft, fftshift
+
+
+def diff(func, start, end, N, order=1):
+    """
+    Return the 1-D derivative of f, of specified order.
+    """
+    L = end - start
+    h = L/N
+    x = np.arange(start+h, end+h, step=h)
+
+    # Compute FFT
+    f = func(x)
+    f_hat = fftshift(2*np.pi*fft(f))/L
+    k = np.arange(-N/2, N/2)
+
+    # Compute derivative
+    f_dot_hat = 1j*k*f_hat
+    f_dot = ifft(fftshift(f_dot_hat))
+
+    return x, f_dot.real
+
+
+def rect(x, lim=1):
+    """
+    Rectangular function
+    """
+    y = np.zeros(x.shape)
+    y[(x > -lim) & (x < lim)] = 1
+
+    if np.any(abs(x) == lim):
+        warnings.warn("WARNING: Sampling rectangular function at discontinuity !")
+
+    return y
+
+
+if __name__ == "__main__":
+
+    # Some function definition
+    f = lambda x: np.exp(np.sin(x))
+
+    # Derive function
+    x, f_dot = diff(rect, -2*np.pi, 2*np.pi, 1e3)
+
+    plt.plot(x, f_dot)
+    # plt.plot(x, f(x)*np.cos(x))
+    plt.show()
