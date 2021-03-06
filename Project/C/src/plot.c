6dd426ed4ba8b7f62191dd2dde7837f7d1c69981
Gilles Poncelet <gillesponcelet98@gmail.com>
2021-03-06 12:24:54 +0100

    better triangle color interpolation

--- Project/C/src/plot.c
+++ Project/C/src/plot.c
@@ -5,34 +5,32 @@ void imshow(bov_window_t *w, double *z, int n1, int n2) {
 
     // Init stuff
     float none[4] = {0.0,0.0,0.0,0.0};
-    float color;
     GLfloat data[5][3];
 
     for (float i = 0.0; i < n1; i++) {
         for (float j = 0.0; j < n2; j++) {
 
             int ind = i*n2+j;
-            color = (float) (z[ind]+1.0)/2.0;
 
             data[0][0] = i;
             data[0][1] = j;
-            data[0][2] = color;
+            data[0][2] = (float) (z[ind]+1.0)/2.0;
 
             data[1][0] = i+1;
             data[1][1] = j;
-            data[1][2] = color;
+            data[1][2] = (float) (z[ind+n2]+1.0)/2.0;
 
             data[2][0] = i+1;
             data[2][1] = j+1;
-            data[2][2] = color;
+            data[2][2] = (float) (z[ind+n2+1]+1.0)/2.0;
 
             data[3][0] = i;
             data[3][1] = j+1;
-            data[3][2] = color;
+            data[3][2] = (float) (z[ind+1]+1.0)/2.0;
 
             data[4][0] = i;
             data[4][1] = j;
-            data[4][2] = color;
+            data[4][2] = (float) (z[ind]+1.0)/2.0;
 
             bov_points_t* points = bov_points_new_with_value(data, 5, GL_STREAM_DRAW);
             bov_points_set_color(points, none);
