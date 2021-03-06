cb0e08593f566f8adccb9e9002269db71469f033
Gilles Poncelet <gillesponcelet98@gmail.com>
2021-03-06 14:03:56 +0100

    cleaner

--- Project/C/src/spectral.c
+++ Project/C/src/spectral.c
@@ -10,7 +10,7 @@
 
 // Shader sources
 const GLchar* vertexSource = R"glsl(
-    #version 150 core
+    #version 460 core
     in vec2 position;
     in float color;
     out float Color;
@@ -20,10 +20,12 @@ const GLchar* vertexSource = R"glsl(
     }
 )glsl";
 const GLchar* fragmentSource = R"glsl(
-    #version 150 core
+    #version 460 core
     in float Color;
+    in vec2 position;
     out vec4 outColor;
-    void main() {
+
+    vec4 turbo(float x) {
         const vec4 kRedVec4 = vec4(0.13572138, 4.61539260, -42.66032258, 132.13108234);
         const vec4 kGreenVec4 = vec4(0.09140261, 2.19418839, 4.84296658, -14.18503333);
         const vec4 kBlueVec4 = vec4(0.10667330, 12.64194608, -60.58204836, 110.36276771);
@@ -31,15 +33,19 @@ const GLchar* fragmentSource = R"glsl(
         const vec2 kGreenVec2 = vec2(4.27729857, 2.82956604);
         const vec2 kBlueVec2 = vec2(-89.90310912, 27.34824973);
 
-        vec4 v4 = vec4( 1.0, Color, Color * Color, Color * Color * Color);
+        vec4 v4 = vec4( 1.0, x, x*x, x*x*x);
         vec2 v2 = v4.zw * v4.z;
 
-        outColor = vec4(
+        return vec4(
             dot(v4, kRedVec4)   + dot(v2, kRedVec2),
             dot(v4, kGreenVec4) + dot(v2, kGreenVec2),
             dot(v4, kBlueVec4)  + dot(v2, kBlueVec2),
             1.0
         );
+    }
+
+    void main() {
+        outColor = turbo(Color);
     }
 )glsl";
 
