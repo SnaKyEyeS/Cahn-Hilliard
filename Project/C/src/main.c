3c5efe55dd1ad834e52df9003b29b70d509ae4bf
Gilles Poncelet <gillesponcelet98@gmail.com>
2021-03-12 18:31:16 +0100

    fix

--- Project/C/src/main.c
+++ Project/C/src/main.c
@@ -66,7 +66,7 @@ int main(int argc, char* argv[]) {
     // Simulation parameters
     int n = N_DISCR;
     int t = 0;
-    double dt = 70.0 / (N*N*N*N);
+    double dt = 70.0 / pow(N_DISCR, 4);
     double skip = 10;
 
     // Initialise Cahn-Hilliard solver
