af4fc763f8b3937cd481ac068b95e43ba621ac43
Gilles Poncelet <gillesponcelet98@gmail.com>
2021-03-14 11:44:46 +0100

    more small optimisations

--- Project/C/src/cpu/solver.c
+++ Project/C/src/cpu/solver.c
@@ -3,6 +3,10 @@
 #define REAL 0
 #define CPLX 1
 
+double hh = 1.0 / (N_DISCR*N_DISCR);
+int nRealElem = N_DISCR*N_DISCR;
+int nCplxElem = N_DISCR*(N_DISCR/2+1);
+
 
 /*
  *  Compute one iteration of Runge Kutta 4
@@ -13,25 +17,25 @@ void RungeKutta4(double* c, double dt){
     f(c, k1);
 
     // K2
-    for(int i = 0; i < N_DISCR*N_DISCR; i++) {
+    for(int i = 0; i < nRealElem; i++) {
         tmp[i] = c[i] + dt*k1[i]/2.0;
     }
     f(tmp, k2);
 
     // K3
-    for(int i = 0; i < N_DISCR*N_DISCR; i++) {
+    for(int i = 0; i < nRealElem; i++) {
         tmp[i] = c[i] + dt*k2[i]/2.0;
     }
     f(tmp, k3);
 
     // K4
-    for(int i = 0; i < N_DISCR*N_DISCR; i++) {
+    for(int i = 0; i < nRealElem; i++) {
         tmp[i] = c[i] + dt*k3[i];
     }
     f(tmp, k4);
 
     // C_i+1
-    for(int i = 0; i < N_DISCR*N_DISCR; i++) {
+    for(int i = 0; i < nRealElem; i++) {
         c[i] += dt*(k1[i] + 2*k2[i] + 2*k3[i] + k4[i])/6.0;
     }
 }
@@ -42,23 +46,25 @@ void RungeKutta4(double* c, double dt){
  */
 void f(double* c, double* dc) {
     // Compute ĉ
-    memcpy(rval, c, N_DISCR*N_DISCR*sizeof(double));
+    memcpy(rval, c, nRealElem*sizeof(double));
     fftw_execute(rfft2);
-    memcpy(c_hat, cval, N_DISCR*(N_DISCR/2+1)*sizeof(fftw_complex));
+    memcpy(c_hat, cval, nCplxElem*sizeof(fftw_complex));
 
     // Compute ĉ³
-    for(int i = 0; i < N_DISCR*N_DISCR; i++) {
+    for(int i = 0; i < nRealElem; i++) {
         rval[i] = c[i]*c[i]*c[i];
     }
     fftw_execute(rfft2);
 
     // Compute F
-    for(int i = 0; i < N_DISCR*(N_DISCR/2+1); i++) {
-        cval[i][REAL] = k[i] * (cval[i][REAL] - c_hat[i][REAL] - A*A*k[i]*c_hat[i][REAL]) / (N_DISCR*N_DISCR);
-        cval[i][CPLX] = k[i] * (cval[i][CPLX] - c_hat[i][CPLX] - A*A*k[i]*c_hat[i][CPLX]) / (N_DISCR*N_DISCR);
+    for(int i = 0; i < nCplxElem; i++) {
+        cval[i][REAL] = k[i] * (cval[i][REAL] - c_hat[i][REAL] - A*A*k[i]*c_hat[i][REAL]);
+        cval[i][CPLX] = k[i] * (cval[i][CPLX] - c_hat[i][CPLX] - A*A*k[i]*c_hat[i][CPLX]);
     }
     fftw_execute(irfft2);
-    memcpy(dc, rval, N_DISCR*N_DISCR*sizeof(double));
+    for (int i = 0; i < nRealElem; i++) {
+        dc[i] = hh*rval[i];
+    }
 }
 
 /*
@@ -66,15 +72,15 @@ void f(double* c, double* dc) {
  */
 void init_solver(double *c) {
     // RK4
-    k1    = (double*) malloc(N_DISCR*N_DISCR*sizeof(double));
-    k2    = (double*) malloc(N_DISCR*N_DISCR*sizeof(double));
-    k3    = (double*) malloc(N_DISCR*N_DISCR*sizeof(double));
-    k4    = (double*) malloc(N_DISCR*N_DISCR*sizeof(double));
-    tmp   = (double*) malloc(N_DISCR*N_DISCR*sizeof(double));
+    k1    = (double*) malloc(nRealElem*sizeof(double));
+    k2    = (double*) malloc(nRealElem*sizeof(double));
+    k3    = (double*) malloc(nRealElem*sizeof(double));
+    k4    = (double*) malloc(nRealElem*sizeof(double));
+    tmp   = (double*) malloc(nRealElem*sizeof(double));
 
     // F
-    c_hat = fftw_alloc_complex(N_DISCR*(N_DISCR/2+1));
-    k = (double*) malloc(N_DISCR*(N_DISCR/2+1)*sizeof(double));
+    c_hat = fftw_alloc_complex(nCplxElem);
+    k = (double*) malloc(nCplxElem*sizeof(double));
     double factor = -4*M_PI*M_PI;
     for (int i = 0; i < N_DISCR; i++) {
         for (int j = 0; j < N_DISCR/2+1; j++) {
@@ -84,8 +90,8 @@ void init_solver(double *c) {
     }
 
     // FFTW
-    cval = fftw_alloc_complex(N_DISCR*(N_DISCR/2+1));
-    rval = fftw_alloc_real(N_DISCR*N_DISCR);
+    cval = fftw_alloc_complex(nCplxElem);
+    rval = fftw_alloc_real(nRealElem);
     rfft2  = fftw_plan_dft_r2c_2d(N_DISCR, N_DISCR, rval, cval, FFTW_PATIENT);
     irfft2 = fftw_plan_dft_c2r_2d(N_DISCR, N_DISCR, cval, rval, FFTW_PATIENT);
 }
