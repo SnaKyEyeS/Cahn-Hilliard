c60aa4ae549cd039d670cba1bbed8024e0a8bab5
Gilles Poncelet <gillesponcelet98@gmail.com>
2021-03-22 17:37:06 +0100

    imex sucks :'(

--- Project/python/test.py
+++ Project/python/test.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 
 from copy import copy
-from spectral import integrate, irfft2, rfft2, n
+from spectral import imex, irfft2, rfft2, n
 
 
 # Load initial & reference solutions
@@ -11,21 +11,21 @@ c_init = rfft2(np.loadtxt("c_128_init.txt").reshape(n, n))
 c_ref = np.loadtxt("c_128_ref.txt").reshape(n, n)
 
 # Initialise stuff
-dts = [1e-6/4, 1e-6/8]
+dts = [1e-6/16, 1e-6/32]
 error = list()
 
 for dt in dts:
     # Time scheme - imex & rk4 as ref
-    imex = integrate(copy(c_init), dt)
+    step = imex(copy(c_init), dt)
     iter = 0
 
     # Iterate to t = 0.001
     print(f"\rStarting iterations for dt = {dt*1e3:.4f} [ms]...")
     while not dt*iter == .001:
-        sol = next(imex)
+        c = next(step)
 
         sys.stdout.write(f"\rt = {dt*iter:.6f} [s]")
         iter += 1
-    error.append(np.sqrt(np.sum((irfft2(sol) - c_ref)**2)))
+    error.append(np.sqrt(np.sum((irfft2(c) - c_ref)**2)))
 
 print(f"\rn_est = {np.log(error[0] / error[1]) / np.log(dts[0]/dts[1])}")
